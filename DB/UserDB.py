5a5baa8910539985b87238b55cf7aab19000fedb
jmanuel2099 <jmanuel2099@gmail.com>
2020-08-04 22:55:18 -0500

    segunda entrega

---
+++ DB/UserDB.py
@@ -0,0 +1,8 @@
+"""from Security.User import User
+
+class UserDB:
+    def __init__(self):
+        self.users =[User("invited", "00000"), User("admin","12345"), User("jose","990920")]
+
+    def getUsers(self):
+        return self.users"""
